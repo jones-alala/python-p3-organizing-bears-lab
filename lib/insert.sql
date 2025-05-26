f875d8f88f342864029816c65410c5cff59f8987
Johnson Alala <jonesalala95@gmail.com>
2025-05-27 01:11:29 +0300

    Organizing Bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 7, 'M', 'brown', 'calm', 1),
+('Rowdy', 6, 'M', 'black', 'aggressive', 1),
+('Tabitha', 5, 'F', 'red', 'friendly', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'serious', 1),
+('Melissa', 4, 'F', 'white', 'playful', 1),
+('Grinch', 3, 'M', 'green', 'grumpy', 0),
+('Wendy', 2, 'F', 'grey', 'gentle', 1),
+(NULL, 1, 'M', 'black', 'mysterious', 1);
